b1a4dddf01ace020b14de97f7b8c4e07e46f72e2
Vincent <vpmcclin@mtu.edu>
2017-10-26 20:34:38 -0400

    renamed ArrowSpawn to the GameController. Added code to both that file and DestroyByBoundary to end and restart the game and include text

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 //Author: Vincent McClintock
 //Date: 10-18-2017
 
-public class ArrowSpawn : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
     public GameObject obstacle;
     public Vector3 spawnValues;
     public int obstacleCount;
@@ -13,10 +16,41 @@ public class ArrowSpawn : MonoBehaviour {
     public float startWait;
     public float waveWait;
 
+    public Text gameOverText;
+    public Text restartText;
+
+    private bool gameOver;
+    private bool restart;
+     
     private void Start()
     {
-        StartCoroutine (SpawnArrows());
+        gameOver = false;
+        restart = false;
+        gameOverText.text = "";
+        restartText.text = ""; 
+        StartCoroutine(SpawnArrows());
     }
+
+    private void Update()
+    {
+        //allows the player to restart by pressing the 'R' key after game over
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+    }
+
+    //called when the player is destroyed, ending the game
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
+    }
+
 
     IEnumerator SpawnArrows()
     {
@@ -25,11 +59,11 @@ public class ArrowSpawn : MonoBehaviour {
         //while loop runs the for loop, constantly spawning waves of arrows
         while (true)
         {
-        	//each for loop spawns a wave of a set number of arrows
+            //each for loop spawns a wave of a set number of arrows
             for (int i = 0; i < obstacleCount; i++)
             {
-            	//Random variable is used in an if, elseif statement in order to have
-            	//arrows spawn in one of three lanes; left, middle, and right.
+                //Random variable is used in an if, elseif statement in order to have
+                //arrows spawn in one of three lanes; left, middle, and right.
                 float xrand = Random.Range(0, 6);
                 int xVal;
                 if (xrand < 2)
@@ -54,8 +88,16 @@ public class ArrowSpawn : MonoBehaviour {
                 Instantiate(obstacle, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
             }
-         	//wait after each for loop to allow time between waves of arrows. 
+            //wait after each for loop to allow time between waves of arrows. 
             yield return new WaitForSeconds(waveWait);
+
+            //breaks out of the wave spawning loop when gameOver is true
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' to Restart";
+                restart = true;
+                break;
+            }
         }
     }
 }
