01d66b7929505e478afd7d49dd5844a0ca8d4135
unknown <dgwarner@mtu.edu>
2017-12-03 00:59:09 -0500

    reworked main menu

---
+++ Assets/Scripts/QuitScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+	public void Quit ()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/SelectScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectScript : MonoBehaviour
+{
+    public EventSystem eSystem;
+    public GameObject objectSelected;
+
+    private bool isSelected;
+
+    //use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxisRaw("Vertical") != 0 && isSelected == false)
+        {
+            eSystem.SetSelectedGameObject(objectSelected);
+            isSelected = true;
+        }
+	}
+
+    private void OnRelease()
+    {
+        isSelected = false;
+    }
+}
